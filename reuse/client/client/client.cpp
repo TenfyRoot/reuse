575eb98285ce8ae2a35c9197ec048511f6e1f402
tenfy11 <tenfy11@163.com>
2018-11-06 20:48:12 +0800

    fix

--- reuse/client/client/client.cpp
+++ reuse/client/client/client.cpp
@@ -25,24 +25,27 @@ int _tmain(int argc, _TCHAR* argv[])
 	WSAStartup(MAKEWORD(1, 1), &wsaData);
 
 	sockfd = socket(2,1,0);
-	sockfd1 = socket(2,1,0);
+	
 	bool breuse = true;
-	setsockopt(sockfd,0xffff,0x0004,(const char*)&breuse,sizeof(bool));
-	setsockopt(sockfd1,0xffff,0x0004,(const char*)&breuse,sizeof(bool));
-
+	setsockopt(sockfd,-1,4,(const char*)&breuse,sizeof(bool));
+	
 	struct sockaddr_in addr;
 	memset(&addr,0,sizeof(struct sockaddr_in));
 	addr.sin_family = 2;
 	addr.sin_addr.s_addr = inet_addr(serveraddr);
 	addr.sin_port = htons(9999);
-
+	
 	connect(sockfd,(struct sockaddr*)&addr,sizeof(struct sockaddr));
 	struct sockaddr_in addrhole;
 	int len = sizeof(struct sockaddr);
 	getsockname(sockfd,(struct sockaddr*)&addrhole,&len);
 
-	printf("client>>bind %s:%d\r\n", inet_ntoa(addrhole.sin_addr),ntohs(addrhole.sin_port));
+	sockfd1 = socket(2,1,0);
+	setsockopt(sockfd1,-1,4,(const char*)&breuse,sizeof(bool));
+	unsigned long dwArg = 1;
+	ioctlsocket(sockfd1,FIONBIO, &dwArg);
 	bind(sockfd1,(struct sockaddr*)&addrhole,sizeof(struct sockaddr));
+	printf("client>>bind %s:%d\r\n", inet_ntoa(addrhole.sin_addr),ntohs(addrhole.sin_port));
 	listen(sockfd1,5);
 
 	unsigned long threadId;
